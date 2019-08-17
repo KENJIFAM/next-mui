cf85a2f0926c9640813086fa013cee6cc7100354
Khoa Pham <kenjifam@gmail.com>
2019-08-17 18:43:46 +0300

    contact section

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,11 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 // import ProTip from '../src/components/ProTip';
 // import Link from '../src/components/Link';
 import MainSection from '../src/containers/Home/MainSection';
-import { container } from '../src/styles/commonStyles';
-import ProductSection from '../src/containers/Home/ProductSection';
+import FeatureSection from '../src/containers/Home/FeatureSection';
+import AboutSection from '../src/containers/Home/AboutSection';
+import RegisterSection from '../src/containers/Home/RegisterSection';
+import ContactSection from '../src/containers/Home/ContactSection';
+import FAQSection from '../src/containers/Home/FAQSection';
 
 const Home: NextPage<{}> = () => {
   const classes = useStyles();
@@ -18,38 +21,34 @@ const Home: NextPage<{}> = () => {
     <>
       <MainSection />
       <div className={clsx(classes.main, classes.mainRaised)}>
-        <div className={classes.container}>
-          <ProductSection />
-          {/* <<Container maxWidth="sm">
-            <Box my={4}>
-              <Typography variant="h4" component="h1" gutterBottom>
-                Next.js with TypeScript example
-              </Typography>
-              <Link href="/about" color="secondary">
-                Go to the about page
-              </Link>
-              <ProTip />
-            </Box>
-          </Container>> */}
-        </div>
+        <FeatureSection />
+        <AboutSection />
+        <RegisterSection />
+        <FAQSection />
+        <ContactSection />
+        {/* <<Container maxWidth="sm">
+          <Box my={4}>
+            <Typography variant="h4" component="h1" gutterBottom>
+              Next.js with TypeScript example
+            </Typography>
+            <Link href="/about" color="secondary">
+              Go to the about page
+            </Link>
+            <ProTip />
+          </Box>
+        </Container>> */}
       </div>
     </>
   );
 };
 
 const useStyles = makeStyles(() => createStyles({
-  container: {
-    zIndex: 12,
-    color: 'inherit',
-    ...container
-  },
   main: {
     background: '#FFFFFF',
     position: 'relative',
     zIndex: 3
   },
   mainRaised: {
-    borderRadius: '6px',
     boxShadow:
       '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)'
   }
---
+++ src/containers/Home/ContactSection.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import createStyles from '@material-ui/core/styles/createStyles';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import LocationOnRounded from '@material-ui/icons/LocationOnRounded';
+import CallRounded from '@material-ui/icons/CallRounded';
+import EmailRounded from '@material-ui/icons/EmailRounded';
+import { GridContainer, GridItem } from '../../components/Grid';
+import GoogleMap from '../../components/GoogleMap';
+import { title, container, primaryColor } from '../../styles/commonStyles';
+
+const ContactSection: React.FC = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <div className={classes.container}>
+        <GridContainer justify="center">
+          <GridItem xs={12}>
+            <h2 className={classes.title}>Contact Us</h2>
+            <div>
+              <GridContainer justify="center" alignItems="stretch">
+                <GridItem xs={12} sm={12} md={4}>
+                  <GridContainer direction="column" justify="center" alignItems="center" className={classes.contactList}>
+                    <GridItem xs={12} className={classes.contact}>
+                      <div className={classes.iconWrapper}>
+                        <LocationOnRounded className={classes.icon} />
+                      </div>
+                      <div className={classes.contactDetail}>
+                        Otakaari 5, 02150 Espoo
+                      </div>
+                    </GridItem>
+                    <GridItem xs={12} className={classes.contact}>
+                      <div className={classes.iconWrapper}>
+                        <CallRounded className={classes.icon} />
+                      </div>
+                      <div className={classes.contactDetail}>
+                        +358 404 567 xxx
+                      </div>
+                    </GridItem>
+                    <GridItem xs={12} className={classes.contact}>
+                      <div className={classes.iconWrapper}>
+                        <EmailRounded className={classes.icon} />
+                      </div>
+                      <div className={classes.contactDetail}>
+                        contact@stepout.fi
+                      </div>
+                    </GridItem>
+                  </GridContainer>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={8}>
+                  <GoogleMap
+                    lat={60.188938}
+                    lng={24.8301198}
+                    name="Otakaari 5, 02150 Espoo"
+                    color={primaryColor}
+                    zoom={15}
+                  />
+                </GridItem>
+              </GridContainer>
+            </div>
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+};
+
+const useStyles = makeStyles(() => createStyles({
+  section: {
+    padding: '70px 0',
+    textAlign: 'center',
+    background: '#f5f5f5'
+  },
+  container: {
+    zIndex: 12,
+    color: 'inherit',
+    ...container
+  },
+  title: {
+    ...title,
+    marginBottom: '4rem',
+    marginTop: '30px',
+    minHeight: '32px',
+    textDecoration: 'none',
+    fontSize: '2.5rem',
+    fontWeight: 500
+  },
+  description: {
+    fontWeight: 300,
+    color: '#444',
+    fontSize: '1.2rem',
+    lineHeight: '2rem'
+  },
+  contactList: {
+    marginBottom: '1rem',
+    height: '100%'
+  },
+  contact: {
+    marginTop: '1.5rem'
+  },
+  iconWrapper: {
+    color: primaryColor
+  },
+  icon: {
+    width: '36px',
+    height: '36px'
+  },
+  contactDetail: {
+    fontSize: '1rem',
+    marginBottom: '1rem',
+    marginTop: '0.5rem'
+  }
+}));
+
+export default ContactSection;
