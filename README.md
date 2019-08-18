513b4b927cb3de0da68ec9da30aa3fc79f5397af
Khoa Pham <kenjifam@gmail.com>
2019-08-18 22:03:18 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Intro
+
+A landing page with Material Design.
+
+Tech stacks: TypeScript, React, Next.js, Material UI, Google App Engine
+
+# Demo
+
+https://next-mui.appspot.com/
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,6 @@ import { NextPage } from 'next/types';
 import clsx from 'clsx';
 import createStyles from '@material-ui/core/styles/createStyles';
 import makeStyles from '@material-ui/core/styles/makeStyles';
-// import Container from '@material-ui/core/Container';
-// import Typography from '@material-ui/core/Typography';
-// import Box from '@material-ui/core/Box';
-// import ProTip from '../src/components/ProTip';
-// import Link from '../src/components/Link';
 import MainSection from '../src/containers/Home/MainSection';
 import FeatureSection from '../src/containers/Home/FeatureSection';
 import AboutSection from '../src/containers/Home/AboutSection';
@@ -26,17 +21,6 @@ const Home: NextPage<{}> = () => {
         <RegisterSection />
         <FAQSection />
         <ContactSection />
-        {/* <<Container maxWidth="sm">
-          <Box my={4}>
-            <Typography variant="h4" component="h1" gutterBottom>
-              Next.js with TypeScript example
-            </Typography>
-            <Link href="/about" color="secondary">
-              Go to the about page
-            </Link>
-            <ProTip />
-          </Box>
-        </Container>> */}
       </div>
     </>
   );
